3dc28505c547f9a263d484f17f2ec47d456aa443
matthieuberger <matthieuberger84@gmail.com>
2021-12-23 16:42:27 +0100

    Fix terraform outputs

--- deployment/terraform/outputs.tf
+++ deployment/terraform/outputs.tf
@@ -1,6 +1,6 @@
 output "users_db_address" {
   description = "Users service database address"
-  value       = module.users_db.db_address
+  value       = module.users_db[0].db_address
 }
 
 output "cert_manager_irsa_role_arn" {
