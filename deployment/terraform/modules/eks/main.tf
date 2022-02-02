640253207cb8df577eb77ccb87cf4c8fbf55bf1d
matthieuberger <matthieuberger84@gmail.com>
2022-02-01 16:44:04 -0800

    Fix terraform modules versions

--- deployment/terraform/modules/eks/main.tf
+++ deployment/terraform/modules/eks/main.tf
@@ -1,11 +1,6 @@
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-}
-
 module "eks" {
   source                 = "terraform-aws-modules/eks/aws"
+  version                = "17.24.0"
   cluster_name           = var.cluster_name
   cluster_version        = "1.21"
   subnets                = var.private_subnets
